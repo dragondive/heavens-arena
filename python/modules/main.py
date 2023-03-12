27f4f259b4bf3c1218f79a57cc82caf05a61e999
Aravind Pai <dragondive@outlook.in>
2023-03-12 10:28:08 +0530

    import the module from other directory

--- python/modules/main.py
+++ python/modules/main.py
@@ -1,14 +1,14 @@
-import message
+import message.message_provider
 
 print(type(message))
 print(dir(message))
-print(message.message_provider.get_message())   # error!
+print(type(message.message_provider))
+print(dir(message.message_provider))
+print(message.message_provider.get_message())
 
-# PS C:\WORK\dragondive\heavens-arena\python\modules> python main.py
+# PS C:\WORK\dragondive\heavens-arena\python\modules> python main.py   
 # <class 'module'>
-# ['__doc__', '__file__', '__loader__', '__name__', '__package__', '__path__', '__spec__']
-# Traceback (most recent call last):
-#   File "C:\WORK\dragondive\heavens-arena\python\modules\main.py", line 5, in <module>
-#     print(message.message_provider.get_message())
-#           ^^^^^^^^^^^^^^^^^^^^^^^^
-# AttributeError: module 'message' has no attribute 'message_provider'
+# ['__doc__', '__file__', '__loader__', '__name__', '__package__', '__path__', '__spec__', 'message_provider']
+# <class 'module'>
+# ['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'get_message']
+# Hello World
