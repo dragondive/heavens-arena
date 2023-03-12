5b2fef1417134875fda1348cdf15aa3ba9c784cc
Aravind Pai <dragondive@outlook.in>
2023-03-12 18:30:59 +0530

    Revert "got back to non-conflicting directory name"
    This reverts commit d12675d18a42dac8fa22115ab5f0b43ac05f30d5.

--- python/modules/main.py
+++ python/modules/main.py
@@ -1,14 +1,14 @@
-import string.message_provider
+import message.message_provider
 
-print(type(string))
-print(dir(string))
-print(type(string.message_provider))
-print(dir(string.message_provider))
-print(string.message_provider.get_message())
+print(type(message))
+print(dir(message))
+print(type(message.message_provider))
+print(dir(message.message_provider))
+print(message.message_provider.get_message())
 
-# PS C:\WORK\dragondive\heavens-arena\python\modules> python main.py
+# PS C:\WORK\dragondive\heavens-arena\python\modules> python main.py   
 # <class 'module'>
-# ['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__path__', '__spec__', 'message_provider']
+# ['__doc__', '__file__', '__loader__', '__name__', '__package__', '__path__', '__spec__', 'message_provider']
 # <class 'module'>
 # ['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'get_message']
 # Hello World
