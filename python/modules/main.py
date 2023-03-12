c5201d46707306894b4681bc62ce9f1223d798bb
Aravind Pai <dragondive@outlook.in>
2023-03-12 19:13:50 +0530

    import from a deeper subdirectory

--- python/modules/main.py
+++ python/modules/main.py
@@ -3,4 +3,10 @@ import message.message_provider
 print(message.message_provider.get_message())
 
 # PS C:\WORK\dragondive\heavens-arena\python\modules> python main.py
+# <class 'module'>
+# ['__doc__', '__file__', '__loader__', '__name__', '__package__', '__path__', '__spec__', 'deep']
+# <class 'module'>
+# ['__doc__', '__file__', '__loader__', '__name__', '__package__', '__path__', '__spec__', 'answer_provider']
+# <class 'module'>
+# ['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'get_answer']
 # Hello 42
--- python/modules/message/message_provider.py
+++ python/modules/message/message_provider.py
@@ -1,4 +1,10 @@
-from answer.answer_provider import get_answer
+import answer.deep.answer_provider
 
 def get_message():
-    return "Hello " + str(get_answer())
+    print(type(answer))
+    print(dir(answer))
+    print(type(answer.deep))
+    print(dir(answer.deep))
+    print(type(answer.deep.answer_provider))
+    print(dir(answer.deep.answer_provider))
+    return "Hello " + str(answer.deep.answer_provider.get_answer())
