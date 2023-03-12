551caffeb74dfe6678d3be894e92d8be2f11317f
Aravind Pai <dragondive@outlook.in>
2023-03-12 10:35:04 +0530

    create a conflict with standard module

--- python/modules/main.py
+++ python/modules/main.py
@@ -1,14 +1,13 @@
-import message.message_provider
+import string.message_provider
 
-print(type(message))
-print(dir(message))
-print(type(message.message_provider))
-print(dir(message.message_provider))
-print(message.message_provider.get_message())
+print(type(string))
+print(dir(string))
+print(type(string.message_provider))
+print(dir(string.message_provider))
+print(string.message_provider.get_message())
 
-# PS C:\WORK\dragondive\heavens-arena\python\modules> python main.py   
-# <class 'module'>
-# ['__doc__', '__file__', '__loader__', '__name__', '__package__', '__path__', '__spec__', 'message_provider']
-# <class 'module'>
-# ['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'get_message']
-# Hello World
+# PS C:\WORK\dragondive\heavens-arena\python\modules> python main.py
+# Traceback (most recent call last):
+#   File "C:\WORK\dragondive\heavens-arena\python\modules\main.py", line 1, in <module>
+#     import string.message_provider
+# ModuleNotFoundError: No module named 'string.message_provider'; 'string' is not a package
