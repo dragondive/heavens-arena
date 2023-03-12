c013b0b113cbc16b9cd9efc03c53c841a76eb25e
Aravind Pai <dragondive@outlook.in>
2023-03-12 21:52:28 +0530

    hack to put import statement at the end

--- python/modules/message/message_provider.py
+++ python/modules/message/message_provider.py
@@ -1,7 +1,7 @@
-import answer.deep.answer_provider
-
 def get_message():
     return "Hello " + str(answer.deep.answer_provider.get_answer())
 
 def get_real_message():
     return "The One Piece is REAL!!!"
+
+import answer.deep.answer_provider
