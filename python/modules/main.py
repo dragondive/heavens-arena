0455423598654fac141da405b77d2722e4a4d440
Aravind Pai <dragondive@outlook.in>
2023-03-12 10:19:47 +0530

    move to another module in same directory

--- python/modules/main.py
+++ python/modules/main.py
@@ -1,4 +1,10 @@
-print("Hello World")
+import message_provider
+
+print(message_provider.get_message())
+print(type(message_provider))
+print(dir(message_provider))
 
 # PS C:\WORK\dragondive\heavens-arena\python\modules> python main.py
 # Hello World
+# <class 'module'>
+# ['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'get_message']
