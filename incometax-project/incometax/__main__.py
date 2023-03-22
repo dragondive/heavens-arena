86273f7281d8329e52b41e8af3d51083f54d098f
Aravind Pai <dragondive@outlook.in>
2023-03-22 13:21:59 +0530

    added module in src/, configured pythonpath in pyproject.toml to find it

--- incometax-project/incometax/__main__.py
+++ incometax-project/incometax/__main__.py
@@ -1,1 +1,5 @@
+from tax_compute_strategy.old_tax_compute_strategy import OldTaxComputeStrategy
+
 print("Hello World")
+tax_compute_strategy = OldTaxComputeStrategy()
+print(tax_compute_strategy.compute())
---
+++ incometax-project/incometax/src/tax_compute_strategy/old_tax_compute_strategy.py
@@ -0,0 +1,6 @@
+class OldTaxComputeStrategy:
+    def __init__(self) -> None:
+        pass
+
+    def compute(self):
+        return 200  # dummy hardcoded value to properly set up the code, will be changed later
