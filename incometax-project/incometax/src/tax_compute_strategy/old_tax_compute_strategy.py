b082f2a2899f1cf756b9f330bf6bb61de015773a
Aravind Pai <dragondive@outlook.in>
2023-03-22 18:46:15 +0530

    completed salary income-related calculation of old tax compute, added unit tests

--- incometax-project/incometax/src/tax_compute_strategy/old_tax_compute_strategy.py
+++ incometax-project/incometax/src/tax_compute_strategy/old_tax_compute_strategy.py
@@ -1,6 +1,51 @@
+import math
+import pandas
+
+
 class OldTaxComputeStrategy:
     def __init__(self) -> None:
-        pass
+        self.tax_brackets = pandas.DataFrame(
+            [  # TODO: Accept this data from yaml file so user can configure it
+                (0, 2_50_000, 0, 0),
+                (2_50_000, 5_00_000, 5, 12_500),
+                (5_00_000, 10_00_000, 20, 1_12_500),
+                (10_00_000, 0, 30, 0),
+            ],
+            columns=["Lower_Limit", "Upper_Limit", "Percent", "Maximum_Tax"],
+        )
 
     def compute(self, income):
-        return income  # dummy implementation to properly set up the code, will be changed later
+        income = math.floor(
+            income
+        )  # Ignore fractional part just as Income Tax website does.
+
+        # Determine brackets whose lower limit is lower than specified income.
+        # The highest among them will be the matching bracket.
+        # As per Income Tax rule, only the income in excess of lower limit in that bracket
+        # will be taxed at the rate of that bracket. In addition, the income until the
+        # lower limit will be taxed at the rates in earlier brackets.
+        # The filtering is done in this manner to avoid filtering two times (once for the
+        # matching bracket, once more for the previous bracket).
+        filtered_brackets = self.tax_brackets[self.tax_brackets["Lower_Limit"] < income]
+
+        matching_bracket = filtered_brackets.iloc[-1]
+        matching_bracket_component = (
+            (income - matching_bracket["Lower_Limit"])
+            * matching_bracket["Percent"]
+            / 100
+        )
+
+        try:
+            previous_bracket = filtered_brackets.iloc[-2]
+        except IndexError:
+            # Special case when matching bracket is the lowest bracket.
+            # Hence, there is no "previous" bracket.
+            previous_bracket_component = 0
+        else:
+            # Income until lower limit is taxed according to previous brackets,
+            # which would simply be the maximum tax in the previous bracket.
+            previous_bracket_component = previous_bracket["Maximum_Tax"]
+
+        # Income Tax website rounds off the computed tax.
+        computed_tax = round(matching_bracket_component + previous_bracket_component)
+        return computed_tax
--- incometax-project/incometax/tests/tax_compute_strategy/test_tax_compute_strategy.py
+++ incometax-project/incometax/tests/tax_compute_strategy/test_tax_compute_strategy.py
@@ -1,8 +1,28 @@
+import pytest
+
 from tax_compute_strategy.old_tax_compute_strategy import OldTaxComputeStrategy
 
 
-def test_old_tax_compute_strategy_compute():
+@pytest.mark.parametrize(
+    "income,expected_tax",
+    [
+        (1_75_000.52, 0),  # check if float values are accepted
+        (2_00_000, 0),  # lowest bracket
+        (2_50_000, 0),  # upper limit of lowest bracket
+        (2_50_001, 0),  # check roundoff near bracket limit
+        (4_13_786, 8189),  # arbitrary number in middle of bracket
+        (4_99_999, 12500),  # near bracket limit
+        (5_00_000, 12500),  # bracket limit
+        (5_00_001, 12500),  # near bracket limit
+        (8_96_247.13, 91749),  # fractional part of tax is 0.4, should round down
+        (8_96_248.83, 91750),  # fractional part of tax is 0.6, should round up
+        (9_99_999, 112500),  # near bracket limit
+        (10_00_000, 112500),  # bracket limit
+        (10_00_001, 112500),  # near bracket limit
+        (15_45_775, 276232),  # arbitrary number in top bracket
+        (25_85_176, 588053),  # arbitrary number in top bracket
+    ],
+)
+def test_old_tax_compute_strategy_compute(income, expected_tax):
     old_tax_compute_strategy = OldTaxComputeStrategy()
-    income = 200
-    expected_tax = 200 # dummy test, will be fixed later
     assert old_tax_compute_strategy.compute(income) == expected_tax
